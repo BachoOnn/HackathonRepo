26caa56db005f9e94237462cef6bfe17fa2492be
BachoOnn <oniani.bachoo@gmail.com>
2026-03-12 14:43:16 +0400

    initial commit

---
+++ 5bit/FiveBitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiveBitApp.swift
+//  5bit
+//
+//  Created by Bacho on 12.03.26.
+//
+
+import SwiftUI
+
+@main
+struct FiveBitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
